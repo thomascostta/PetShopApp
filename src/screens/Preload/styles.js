28e354bfbcf8af382d3ccda5e869f40430d7a51f
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-22 10:14:26 -0300

    first commit

---
+++ src/screens/Preload/styles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  /* background-color: #63c2d1; */
+  background-color: #38d9a9;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
