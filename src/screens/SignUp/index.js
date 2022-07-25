ca6a6d0d14e47b27cff31f02c41cf7ec3a1ba0e6
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-25 18:39:59 -0300

    SignUp

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,11 +1,86 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtomText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import Api from '../../api';
+
+import PetShop from '../../assets/house-pet.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+import PersonIcon from '../../assets/person.svg';
+
+import SignInput from '../../components/SignInput';
 
 export default () => {
+  const navigation = useNavigation();
+  const [nameField, setNameField] = useState('');
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  const handleSignClick = async () => {
+    if (nameField != '' && emailField != '' && passwordField != '') {
+      let res = await Api.singUp(nameField, emailField, passwordField);
+
+      if (res.token) {
+        alert('DEU CERTO');
+      } else {
+        alert('Erro: ' + res.error);
+      }
+    } else {
+      alert('Preencha os campos');
+    }
+  };
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignIn'}],
+    });
+  };
+
   return (
     <Container>
-      <Text>SignUp</Text>
+      <PetShop width="100%" height="160" />
+
+      <InputArea>
+        <SignInput
+          IconSvg={PersonIcon}
+          placeholder="Digite seu nome"
+          value={nameField}
+          onChangeText={(text) => setNameField(text)}
+        />
+
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={(text) => setEmailField(text)}
+        />
+
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          email={passwordField}
+          onChangeText={(text) => setPasswordField(text)}
+          password={true}
+        />
+
+        <CustomButton onPress={handleSignClick}>
+          <CustomButtomText>CADASTRAR</CustomButtomText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton onPress={handleMessageButtonClick}>
+        <SignMessageButtonText>Já possui uma conta?</SignMessageButtonText>
+        <SignMessageButtonTextBold> Faça Login</SignMessageButtonTextBold>
+      </SignMessageButton>
     </Container>
   );
 };
