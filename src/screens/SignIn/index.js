368c7357abfe2e4594876c68d7cae492f6bca002
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-25 18:40:13 -0300

    SignIn

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,83 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtomText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import Api from '../../api';
+
+import SignInput from '../../components/SignInput';
+
+import PetShop from '../../assets/house-pet.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+  const navigation = useNavigation();
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+// suport@b7web.com.br
+// 1234
+
+  const handleSignClick = async () => {
+    if (emailField != '' && passwordField != '') {
+      let json = await Api.signIn(emailField, passwordField);
+      console.log(json);
+      
+      if (json.token) {
+        alert('DEU CERTO');
+      } else {
+        alert('E-mail e/ou senha errados!');
+      }
+    } else {
+      alert('Preencha os Campos');
+    }
+  };
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignUp'}],
+    });
+  };
+
   return (
     <Container>
-      <Text>SignIn</Text>
+      <PetShop width="100%" height="160" />
+
+      <InputArea>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={(text) => setEmailField(text)}
+        />
+
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          email={passwordField}
+          onChangeText={(text) => setPasswordField(text)}
+          password={true}
+        />
+
+        <CustomButton onPress={handleSignClick}>
+          <CustomButtomText>LOGIN</CustomButtomText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton onPress={handleMessageButtonClick}>
+        <SignMessageButtonText>
+          Ainda não possui uma conta?
+        </SignMessageButtonText>
+        <SignMessageButtonTextBold> Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
     </Container>
   );
 };
