42886feaa03730486a349600461b2b30abb5ab0d
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-26 15:55:17 -0300

    settings

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,18 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Container, LoadingIcon} from './styles';
 import AsyncStorage from '@react-native-community/async-storage';
 import {useNavigation} from '@react-navigation/native';
 
+import {UserContext} from '../../contexts/UserContext';
+
+import Api from '../../api';
+
 import PetShop from '../../assets/house-pet.svg';
 
 export default () => {
+  const {dispatch: userDispatch} = useContext(UserContext);
   const navigation = useNavigation();
 
   useEffect(() => {
     const checkToken = async () => {
       const token = await AsyncStorage.getItem('token');
       if (token) {
-        // validar o token
+        let res = await Api.checkToken(token);
+        if (token) {
+          await AsyncStorage.setItem('token', res.token);
+
+          userDispatch({
+            type: 'setAvatar',
+            payload: {
+              avatar: res.data.avatar,
+            },
+          });
+
+          navigation.reset({
+            routes: [{name: 'MainTab'}],
+          });
+        }
       } else {
         navigation.navigate('SignIn');
       }
