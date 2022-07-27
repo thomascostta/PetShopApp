7bd37ee7d7ad2630b28549aa4d452b0cb1a3ce21
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-27 17:55:24 -0300

    create

---
+++ src/components/BarberItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import Stars from '../components/Stars';
+
+const Area = styled.TouchableOpacity`
+  background-color: #ffffff;
+  margin-bottom: 20px;
+  border-radius: 20px;
+  padding: 14px;
+  flex-direction: row;
+`;
+
+const Avatar = styled.Image`
+  width: 88px;
+  height: 88px;
+  border-radius: 20px;
+`;
+
+const InfoArea = styled.View`
+  margin-left: 20px;
+  justify-content: space-between;
+`;
+
+const UserName = styled.Text`
+  font-size: 17px;
+  font-weight: bold;
+`;
+
+const SeeProfileButton = styled.View`
+  width: 85px;
+  height: 26px;
+  border: 1px solid #4eadbe;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SeeProfileButtonText = styled.Text`
+  font-size: 13px;
+  color: #268596;
+`;
+
+export default ({data}) => {
+  return (
+    <Area>
+      <Avatar source={{uri: data.avatar}} />
+      <InfoArea>
+        <UserName>{data.name}</UserName>
+
+        <Stars stars={data.stars} showNumber={true} />
+
+        <SeeProfileButton>
+          <SeeProfileButtonText>Ver perfil</SeeProfileButtonText>
+        </SeeProfileButton>
+      </InfoArea>
+    </Area>
+  );
+};
