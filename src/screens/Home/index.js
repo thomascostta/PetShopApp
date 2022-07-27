752dfe8b2e35ee10ea3c89ca452192a9d6885dfb
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-27 10:11:42 -0300

    settings

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
+  const [locationText, setLocationText] = useState('');
+  const [coords, setCoords] = useState(null);
+
+  const handleLocationFinder = () => {
+    setCoords(null);
+  };
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre o seu Veterinário favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
+            <SearchIcon width="26" heigth="26" fill="#ffffff" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#ffffff"
+            value={locationText}
+            onChangeText={(text) => setLocationText(text)}
+          />
+          <LocationFinder onPress={handleLocationFinder}>
+            <MyLocationIcon width="26" heigth="26" fill="#ffffff" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 };
