30f90ad1c0f94e6117645459d1d78e8c66d451b6
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-28 18:00:50 -0300

    settings

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {Platform, RefreshControl} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
+
+import Api from '../../api';
 
 import {
-  Container,
   Scroller,
+  ListArea,
+  Container,
   HeaderArea,
+  LoadingIcon,
   HeaderTitle,
   SearchButton,
   LocationArea,
   LocationInput,
   LocationFinder,
 } from './styles';
+
+import BarberItem from '../../components/BarberItem';
 
 import SearchIcon from '../../assets/search.svg';
 import MyLocationIcon from '../../assets/my_location.svg';
@@ -20,14 +29,76 @@ export default () => {
 
   const [locationText, setLocationText] = useState('');
   const [coords, setCoords] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const handleLocationFinder = () => {
+  useEffect(() => {
+    getBarbers();
+  }, []);
+
+  const handleLocationFinder = async () => {
     setCoords(null);
+    let result = await request(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+    );
+
+    if (result === 'granted') {
+      setLoading(true);
+      setLocationText('');
+      setList([]);
+
+      Geolocation.getCurrentPosition((info) => {
+        setCoords(info.coords);
+        getBarbers();
+      });
+    }
+  };
+
+  const getBarbers = async () => {
+    setLoading(true);
+    setList([]);
+
+    let lat = null;
+    let lng = null;
+
+    if (coords) {
+      lat = coords.latitude;
+      lng = coords.longitude;
+    }
+
+    let res = await Api.getBarbers(lat, lng, locationText);
+
+    if (res.error == '') {
+      if (res.loc) {
+        setLocationText(res.loc);
+      }
+
+      setList(res.data);
+    } else {
+      alert('Erro:  ', res.error);
+    }
+    setLoading(false);
+  };
+
+  const onRefresh = () => {
+    setRefreshing(false);
+    getBarbers();
+  };
+
+  const handleLocationSearch = () => {
+    setCoords({});
+    getBarbers();
   };
 
   return (
     <Container>
-      <Scroller>
+      <Scroller
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
         <HeaderArea>
           <HeaderTitle numberOfLines={2}>
             Encontre o seu Veterinário favorito
@@ -43,11 +114,20 @@ export default () => {
             placeholderTextColor="#ffffff"
             value={locationText}
             onChangeText={(text) => setLocationText(text)}
+            onEndEditing={handleLocationSearch}
           />
           <LocationFinder onPress={handleLocationFinder}>
             <MyLocationIcon width="26" heigth="26" fill="#ffffff" />
           </LocationFinder>
         </LocationArea>
+
+        {loading && <LoadingIcon size="large" color="#ffffff" />}
+
+        <ListArea>
+          {list.map((item, index) => (
+            <BarberItem key={index} data={item} />
+          ))}
+        </ListArea>
       </Scroller>
     </Container>
   );
