3a275b00e0ade79099ad1529fac3ec0c0cb5ee28
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-01 17:12:32 -0300

    imagePet

---
+++ src/data/imagePet.js
@@ -0,0 +1,18 @@
+export const imagePet = [
+  {
+    id: '1',
+    image: require('../assets/pet1.png'),
+  },
+  {
+    id: '2',
+    image: require('../assets/pet2.png'),
+  },
+  {
+    id: '3',
+    image: require('../assets/pet3.png'),
+  },
+  {
+    id: '4',
+    image: require('../assets/pet4.png'),
+  },
+];
