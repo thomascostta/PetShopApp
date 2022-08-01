252ac0c9b7801f82e903a1a37f2e833b3003081f
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-01 17:13:01 -0300

    settings

--- src/components/BarberItem.js
+++ src/components/BarberItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import {useNavigation} from '@react-navigation/native';
 
 import Stars from '../components/Stars';
 
@@ -42,8 +43,19 @@ const SeeProfileButtonText = styled.Text`
 `;
 
 export default ({data}) => {
+  const navigation = useNavigation();
+
+  const handleClick = () => {
+    navigation.navigate('Barber', {
+      id: data.id,
+      avatar: data.avatar,
+      name: data.name,
+      stars: data.stars,
+    });
+  };
+
   return (
-    <Area>
+    <Area onPress={handleClick}>
       <Avatar source={{uri: data.avatar}} />
       <InfoArea>
         <UserName>{data.name}</UserName>
