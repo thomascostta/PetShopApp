ce084a864a0c0026a837feabfd92dd39294a9cce
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-01 17:12:42 -0300

    settings

--- src/api.js
+++ src/api.js
@@ -65,4 +65,14 @@ export default {
     const json = await req.json();
     return json;
   },
+
+  getBarber: async (id) => {
+    const token = await AsyncStorage.getItem('token');
+
+    const req = await fetch(`${BASE_API}/barber/${id}?token=${token}`);
+    const json = await req.json();
+    console.log('json');
+    
+    return json;
+  },
 };
