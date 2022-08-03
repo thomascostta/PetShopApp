4cb9d68f21f55b487d2bc161ebbbe570fb5e5390
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-03 15:28:23 -0300

    setup

--- src/api.js
+++ src/api.js
@@ -72,7 +72,22 @@ export default {
     const req = await fetch(`${BASE_API}/barber/${id}?token=${token}`);
     const json = await req.json();
     console.log('json');
-    
+
+    return json;
+  },
+
+  setFavorite: async (barberId) => {
+    const token = await AsyncStorage.getItem('token');
+
+    const req = await fetch(`${BASE_API}/favorite`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({barber: barberId}),
+    });
+    const json = await req.json();
     return json;
   },
 };
