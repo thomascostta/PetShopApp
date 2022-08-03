f0d6711b5c03af126dfbce6554fd485ed66a2bca
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-03 15:27:07 -0300

    settings

--- src/screens/Barber/styles.js
+++ src/screens/Barber/styles.js
@@ -83,13 +83,91 @@ export const UserFavButton = styled.TouchableOpacity`
   margin-right: 20px;
 `;
 
-export const ServiceArea = styled.View``;
+export const ServiceArea = styled.View`
+  margin-top: 30px;
+`;
 
-export const TestimonialArea = styled.View``;
+export const ServiceItem = styled.View`
+  flex-direction: row;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-bottom: 20px;
+`;
+
+export const ServiceInfo = styled.View`
+  flex: 1;
+`;
+
+export const ServiceName = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #268596;
+`;
+
+export const ServicePrice = styled.Text`
+  font-size: 14px;
+  color: #268596;
+`;
+
+export const ServiceChooseButton = styled.TouchableOpacity`
+  background-color: #4eadbe;
+  border-radius: 10px;
+  padding: 10px 15px;
+`;
+
+export const ServiceChooseBtnText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+export const ServicesTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #268596;
+  margin-left: 30px;
+  margin-bottom: 20px;
+`;
+
+export const TestimonialArea = styled.View`
+  margin-top: 30px;
+  margin-bottom: 50px;
+`;
+
+export const TestimonialItem = styled.View`
+  background-color: #268596;
+  padding: 15px;
+  border-radius: 10px;
+  height: 110px;
+  justify-content: center;
+  margin-left: 50px;
+  margin-right: 50px;
+`;
+
+export const TestimonialInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 5px;
+`;
+
+export const TestimonialName = styled.Text`
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const TestimonialBody = styled.Text`
+  color: #ffffff;
+  font-size: 13px;
+`;
 
 export const BackButton = styled.TouchableOpacity`
   position: absolute;
   left: 0;
   top: 0;
   z-index: 9;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
 `;
