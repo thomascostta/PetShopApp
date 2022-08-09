7533cdeea356bb2011ff96ca570ac3906a66948e
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-09 14:26:19 -0300

    settings context

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -4,10 +4,11 @@ import {initialState, UserReducer} from '../reducers/UserReducer';
 export const UserContext = createContext();
 
 export default ({children}) => {
-  const [state, dispatch] = useReducer(UserReducer, initialState);
-  return (
-    <UserContext.Provider value={{state, dispatch}}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+    const [state, dispatch] = useReducer(UserReducer, initialState); 
+
+    return (
+        <UserContext.Provider value={{state, dispatch}} >
+            {children}
+        </UserContext.Provider>
+    );
+}
--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,15 +1,20 @@
-export default initialState = {
-  avatar: '',
-  favorites: [],
-  appointments: [],
+export const initialState = {
+    avatar:'',
+    name: '',
+    email: '',
+    favorites:[],
+    appointments:[]
 };
 
 export const UserReducer = (state, action) => {
-  switch (action.type) {
-    case 'setAvatar':
-      return {...state, avatar: action.payload.avatar};
-      break;
-    default:
-      return state;
-  }
-};
+    switch(action.type) {
+        case 'setAvatar':
+            return {...state, 
+                avatar: action.payload.avatar, 
+                name: action.payload.name, 
+                email: action.payload.email};
+            break;
+        default:
+            return state;
+    }
+}
